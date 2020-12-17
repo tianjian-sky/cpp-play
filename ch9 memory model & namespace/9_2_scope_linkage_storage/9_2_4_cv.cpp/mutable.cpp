d94171f464b4400a39168a530d46a5c8c121b900
lrz <lrz_china@163.com>
2020-12-17 12:17:41 +0800

    note

--- ch9 memory model & namespace/9_2_scope_linkage_storage/9_2_4_cv.cpp/mutable.cpp
+++ ch9 memory model & namespace/9_2_scope_linkage_storage/9_2_4_cv.cpp/mutable.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 const int Ar_size = 10;
 
+/**
+ * mutable的作用有两点：
+    （1）保持常量对象中大部分数据成员仍然是“只读”的情况下，实现对个别数据成员的修改；
+    （2）使类的const函数可以修改对象的mutable数据成员。
+
+ * 使用mutable的注意事项：
+    （1）mutable只能作用于类的非静态和非常量数据成员。
+    （2）在一个类中，应尽量或者不用mutable，大量使用mutable表示程序设计存在缺陷。
+*/
+
 struct data {
     char name[30];
     mutable int accesses;
